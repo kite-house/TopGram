6e1cfb13f98ffa71ccaf2799b4c0426ae65dbd91
kite-house <hodosok1301@gmail.com>
2024-04-17 08:04:54 +0700

    fixbug

--- topgram/home/userData.py
+++ topgram/home/userData.py
@@ -14,11 +14,7 @@ class UserData:
         except Exception:
             recipient_user = Users.objects.get(username = request.user)
 
-        if 'user_timezone' in request.session:
-            request.session['user_timezone']
-
-        else:
-            request.session['user_timezone'] = 'Europe/Dublin'
+        request.session.setdefault('user_timezone', 'Europe/Dublin')
 
         self.request = request
         self.user = user
@@ -40,44 +36,29 @@ class UserData:
 
     def data(self):
         ''' Вывод данных в html шаблон '''
-
         try:
             messages = self.owner_user.messages['recipient_name'][self.recipient_user.username]
+            messages_edit = [{
+                "id": message['id'],
+                "time": self.display_time(message['time']),
+                'sender_status': 'owner' if message['sender_name'] == self.owner_user.username else '',
+                "sender_avatar": Users.objects.get(username=message['sender_name']).avatar,
+                "content": self.fernet.decrypt(message['content']).decode()
+            } for message in messages]
         except Exception:
             messages_edit = {}
-        else:
-            messages_edit = []
-            for message in messages:
-                if message['sender_name'] == self.owner_user.username:
-                    sender_status = 'owner'
-
-                else:
-                    sender_status = ''
-                messages_edit.append({
-                    "id" : message['id'],
-                    "time" : self.display_time(message['time']),
-                    'sender_status' : sender_status,
-                    "sender_avatar" : Users.objects.get(username = message['sender_name']).avatar,
-                    "content" : self.fernet.decrypt(message['content']).decode()
-                })
 
 
-        chat_list = []
-        for chat in self.owner_user.chat_list:
-            chat_user = Users.objects.get(username = chat['name'])
+        chat_list = [{
+            "id": chat['id'],
+            'username': chat['name'],
+            'name': Users.objects.get(username=chat['name']).display_name,
+            "avatar": Users.objects.get(username=chat['name']).avatar,
+            "is_online": Users.objects.get(username=chat['name']).is_online(),
+            "last_msg": self.fernet.decrypt(chat['last_msg']).decode(),
+            "last_time_msg": self.display_time(chat['last_time_msg'])
+        } for chat in self.owner_user.chat_list]
 
-            chat_list.append({
-                "id" : chat['id'],
-                'username' : chat['name'],
-                'name' : Users.objects.get(username = chat['name']).display_name,
-                "avatar" : chat_user.avatar,
-                "is_online" : chat_user.is_online(),
-                "last_msg" : self.fernet.decrypt(chat['last_msg']).decode(),
-                "last_time_msg" : self.display_time(chat['last_time_msg'])
-            })
-        
-        if self.recipient_user == None:
-            self.recipient_user.username = "None"
 
         return {
             "owner_name" : self.owner_user.username,
@@ -92,47 +73,37 @@ class UserData:
     
     def update_chat_list(self, msg, timeMsg):
         ''' Обновление чат листа у отправителя и получателя сообщений '''
-        # Проверка на присуствие в чат листе
 
-        # Отправитель
-        if list(map(lambda name: name['name'], self.owner_user.chat_list)).count(self.recipient_user.username) != 0:
-            for chat in self.owner_user.chat_list:
-                if self.recipient_user.username in chat['name']:
-                    self.owner_user.chat_list.remove(chat)
+        def remove_from_chat_list(user, target_user):
+            user.chat_list = [chat for chat in user.chat_list if chat['name'] != target_user.username]
 
-        # Получатель
-        if list(map(lambda name: name['name'], self.recipient_user.chat_list)).count(self.owner_user.username) != 0:
-            for chat in self.recipient_user.chat_list:
-                if self.owner_user.username in chat['name']:
-                    self.recipient_user.chat_list.remove(chat)
+        def update_chat_list_ids(chat_list):
+            for idx, item in enumerate(chat_list, start=1):
+                item['id'] = idx
 
-        # обновляем все ид
+        def update_chat_list_entry(user, target_user, encrypted_msg, timeMsg):
+            user.chat_list.append({
+                "id": len(user.chat_list) + 1,
+                "name": target_user.username,
+                "avatar": target_user.avatar,
+                "last_msg": self.fernet.encrypt(encrypted_msg.encode()).decode(),
+                "last_time_msg": timeMsg
+            })
 
-        for idx, item in enumerate(self.owner_user.chat_list, start=1):
-            item['id'] = idx
 
-        for idx, item in enumerate(self.recipient_user.chat_list, start=1):
-            item['id'] = idx
-
-        # Изменение чат листа у отправителя.
-
-        self.owner_user.chat_list.append({
-            "id": len(self.owner_user.chat_list) + 1,
-            "name": self.recipient_user.username,
-            "avatar":self.recipient_user.avatar,
-            "last_msg": self.fernet.encrypt(f'Вы: {msg}'.encode()).decode(),
-            "last_time_msg": timeMsg
-        })
-
-        # Изменение чат листа у получателя
-
-        self.recipient_user.chat_list.append({
-            "id": len(self.recipient_user.chat_list) + 1,
-            "name": self.owner_user.username,
-            "avatar":self.owner_user.avatar,
-            "last_msg": self.fernet.encrypt(msg.encode()).decode(),
-            "last_time_msg": timeMsg
-        })
+        # Удаление из чат-листов
+        remove_from_chat_list(self.owner_user, self.recipient_user)
+        remove_from_chat_list(self.recipient_user, self.owner_user)
+        
+        # Обновление идентификаторов
+        update_chat_list_ids(self.owner_user.chat_list)
+        update_chat_list_ids(self.recipient_user.chat_list)
+        
+        # Изменение чат-листа у отправителя
+        update_chat_list_entry(self.owner_user, self.recipient_user, f'Вы: {msg}', timeMsg)
+        
+        # Изменение чат-листа у получателя
+        update_chat_list_entry(self.recipient_user, self.owner_user, msg, timeMsg)
 
     def send_message(self):
         ''' Отправка и получение сообщений '''
@@ -180,18 +151,15 @@ class UserData:
         owner_messages = self.owner_user.messages['recipient_name'][self.recipient_user.username]
         recipient_messages = self.recipient_user.messages['recipient_name'][self.owner_user.username]
 
-        if message_id < len(owner_messages):
-            owner_messages.pop(message_id)
-            for idx, item in enumerate(owner_messages, start=1):
-                item['id'] = idx
-            self.owner_user.save()
 
-        if message_id < len(recipient_messages):
-            recipient_messages.pop(message_id)
-            for idx, item in enumerate(recipient_messages, start=1):
-                item['id'] = idx
-            self.recipient_user.save()
+        def delete_msg_and_update_id(user, message_id):
+            if message_id < len(user):
+                user.pop(message_id)
+                for idx, item in enumerate(user, start=1):
+                    item['id'] = idx
 
+        delete_msg_and_update_id(owner_messages, message_id)
+        delete_msg_and_update_id(recipient_messages, message_id)
 
         # Если сообщение последнее, то удаляем из чат листа.
         if message_id == len(owner_messages) and message_id  == len(recipient_messages) :
@@ -200,43 +168,39 @@ class UserData:
             else:
                 self.update_chat_list(" ", " ")
 
-            self.owner_user.save()
-            self.recipient_user.save()
+        self.owner_user.save()
+        self.recipient_user.save()
         
-
     def delete_chat(self):
         ''' Удаление чатов '''
         chat_id = int(self.request.POST.get("delete_chat")) - 1
-
         owner_chat = self.owner_user.chat_list[chat_id]
-
-
-        recipient_chat = Users.objects.get(username = owner_chat['name']).chat_list
+        recipient_user = Users.objects.get(username = owner_chat['name'])
 
         try:
-            index = next(index for index, item in enumerate(recipient_chat) if item["name"] == self.owner_user.username)
+            index = next(index for index, item in enumerate(recipient_user.chat_list) if item["name"] == self.owner_user.username)
         except StopIteration:
             index = 0
 
-        if len(self.owner_user.chat_list) > 0:
+        if len(self.owner_user.chat_list) >= 0 and len(self.owner_user.chat_list) > chat_id:
             self.owner_user.chat_list.pop(chat_id)
-        if len(self.recipient_user.chat_list) > 0:
+        if len(self.recipient_user.chat_list) >= 0 and len(self.owner_user.chat_list) > index:
             self.recipient_user.chat_list.pop(index)
 
+        def update_id(user):
+            for idx, item in enumerate(user.chat_list, start=1):
+                item['id'] = idx
 
-        for idx, item in enumerate(self.owner_user.chat_list, start=1):
-            item['id'] = idx
+        update_id(self.owner_user)
+        update_id(recipient_user)
 
-        for idx, item in enumerate(self.recipient_user.chat_list, start=1):
-            item['id'] = idx
-
-        if self.recipient_user.username in self.owner_user.messages['recipient_name']:
-            del self.owner_user.messages['recipient_name'][self.recipient_user.username]
-        if self.owner_user.username in self.recipient_user.messages['recipient_name']:
-            del self.recipient_user.messages['recipient_name'][self.owner_user.username]
+        if recipient_user.username in self.owner_user.messages['recipient_name']:
+            del self.owner_user.messages['recipient_name'][recipient_user.username]
+        if self.owner_user.username in recipient_user.messages['recipient_name']:
+            del recipient_user.messages['recipient_name'][self.owner_user.username]
 
         self.owner_user.save()
-        self.recipient_user.save()
+        recipient_user.save()
 
 
 
