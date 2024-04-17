80e12b41589f5523e44e02064605c436e3464246
kite-house <hodosok1301@gmail.com>
2024-04-18 03:58:41 +0700

    deployming_env

--- topgram/topgram/settings.py
+++ topgram/topgram/settings.py
@@ -15,12 +15,11 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_path = join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
