14860f1fcd600824f69bcef8798583c756ef668b
kite-house <hodosok1301@gmail.com>
2024-04-18 08:32:26 +0700

    ad

--- topgram/settings.py
+++ topgram/settings.py
@@ -32,9 +32,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get("DOMEN")]
+ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://*.topgram.com']
+CSRF_TRUSTED_ORIGINS = ['https://topgram-production.up.railway.app']
 
 # Application definition
 
