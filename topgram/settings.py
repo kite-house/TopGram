b4542d14290aa010b90948eeff04bca49e8878b8
kite-house <88319088+kite-house@users.noreply.github.com>
2024-04-18 10:22:30 +0700

    Update settings.py

--- topgram/settings.py
+++ topgram/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://topgram.railway.internal",
+    "https://topgram-production.up.railway.app",
+    "https://topgram.app",
+    "http://127.0.0.1:8000"]
 
 CSRF_TRUSTED_ORIGINS = ['https://topgram-production.up.railway.app']
 
